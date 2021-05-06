a76a3ba1dfad7da834a11b4ce914357364f1ab7e
Deepak Malik <deepak164malik@gmail.com>
2021-05-07 00:18:56 +0530

    updated app.js

--- app.js
+++ app.js
@@ -2,11 +2,28 @@ var btnTranslate=document.querySelector("#btn-translate");
 var txtInput= document.querySelector("#txt-input")
 var outputDiv =document.querySelector("#output")
 
+var serverURL="https://api.funtranslations.com/translate/minion.json"
 
+function getTranslationURl(text){
+    return serverURL+"?"+"text="+text
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Something wrong with the server! please try again after sometime")
+}
 
 
 function clickEventHandler(){
-    outputDiv.innerText="Deepak "+ txtInput.value;  
+    var inputText= txtInput.value
+
+    fetch(getTranslationURl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translated =json.contents.translated;
+        outputDiv.innerText=translated ;})
+    .catch(errorHandler)
+    
 }
 
 
